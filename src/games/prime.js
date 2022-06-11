be8caf48fa8106c00599c55bfa825eb18dbd7b31
Frekby <akulinsky@ya.ru>
2022-06-11 20:05:55 +0700

    refactoring game

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,47 +6,35 @@ import {
   reportFail,
   reportWin,
 } from '../index.js';
+import getRandomNum from '../getRandomNum.js';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+
+  const maxDelimiter = num / 2;
+
+  for (let delimiter = 2; delimiter <= maxDelimiter; delimiter += 1) {
+    if (num % delimiter === 0) return false;
+  }
+
+  return true;
+};
 
 const primeGame = () => {
-  const primeNum = [
-    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-  ];
-  let count = 0;
-  let goodCount = 0;
-  let ans;
   const userName = greeting();
   console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-  while (count < 3) {
-    const randomNum = Math.floor(Math.random() * primeNum.at(-1) + 1);
-    sendQuestion(randomNum);
-    ans = getAnswer();
-    const availableAnswers = ['yes', 'no'];
-
-    if (primeNum.includes(randomNum) && !availableAnswers.includes(ans)) {
-      reportFail(ans, 'yes', userName);
-      break;
-    } else if (
-      !primeNum.includes(randomNum) && !availableAnswers.includes(ans)
-    ) {
-      reportFail(ans, 'no', userName);
-      break;
+  for (let x = 0; x < 3; x += 1) {
+    const num = getRandomNum(1, 10);
+    sendQuestion(num);
+    const userAnswer = getAnswer();
+    const correctAnswer = isPrime(num) ? 'yes' : 'no';
+    if (userAnswer !== correctAnswer) {
+      reportFail(userAnswer, correctAnswer, userName);
+      return;
     }
-
-    if (primeNum.includes(randomNum) && ans === 'yes') {
-      reportGoodStep();
-      goodCount += 1;
-    } else if (!primeNum.includes(randomNum) && ans !== 'yes') {
-      reportGoodStep();
-      goodCount += 1;
-    } else {
-      reportFail(ans, ans === 'yes' ? 'no' : 'yes', userName);
-      break;
-    }
-    count += 1;
+    reportGoodStep();
   }
-  if (goodCount === 3) {
-    reportWin(userName);
-  }
+  reportWin(userName);
 };
 export default primeGame;
