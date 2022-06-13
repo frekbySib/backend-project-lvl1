3e5c663667fa197ed2664caa90bf869052a58449
Frekby <akulinsky@ya.ru>
2022-06-13 11:46:19 +0700

    add uniq for fix codeclimate warnings

--- src/games/even.js
+++ src/games/even.js
@@ -6,12 +6,13 @@ const isEven = (num) => num % 2 === 0;
 const evenGame = () => {
   const questionsAndAnswers = [];
   const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-  for (let questionNum = 0; questionNum < questionsCount; questionNum += 1) {
+  let step = 0;
+  while (step < questionsCount) {
     const num = getRandomNum(1, 30);
     const question = num;
     const answer = isEven(num) ? 'yes' : 'no';
     questionsAndAnswers.push([question, answer]);
+    step += 1;
   }
 
   playGame(rule, questionsAndAnswers);
